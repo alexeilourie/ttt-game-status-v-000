00b38054f11ff1739044da42890d08dcb73f9ec4
Alexei Lourie <alexeilourie@gmail.com>
2018-08-28 15:21:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,31 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+def won?(board)
+    WIN_COMBINATIONS.each do |comb|
+    if comb[0] == comb[1] && comb[0] == comb[2]
+      return comb
+    end 
+  end
+  return false
+end
+
+def full?(board)
+  board.each do |cell|
+    if cell == " " || cell == "" || cell == nil
+      return false
+    end
+  end
+  return true
+end
